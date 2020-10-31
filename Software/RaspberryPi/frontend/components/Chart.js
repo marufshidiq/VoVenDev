9c100901be5aec4c48a62f27160509349f482864
Ma'ruf Shidiq <maruf.shidiq@mail.ugm.ac.id>
2020-10-31 05:36:18 +0000

    Can display Chart with proper content layout

---
+++ Software/RaspberryPi/frontend/components/Chart.js
@@ -0,0 +1,61 @@
+import React from "react";
+import styles from "../styles/variables.scss";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+export default function Chart(props) {
+  const { height, width } = props;
+  const data = [
+    {
+      name: "Page A",
+      uv: 4000,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+    },
+  ];
+  return (
+    <ResponsiveContainer width={width} height={height}>
+      <AreaChart data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Area
+          type="monotone"
+          dataKey="uv"
+          stroke={styles.primary_white}
+          fill={styles.primary_blue}
+        />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
+}
--- Software/RaspberryPi/frontend/pages/index.js
+++ Software/RaspberryPi/frontend/pages/index.js
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import RealTimeBox from "../components/RealTimeBox";
 import Button from "../components/Button";
+import Chart from "../components/Chart";
 import MaleIcon from "../assets/Icons/MaleIcon";
 import FemaleIcon from "../assets/Icons/FemaleIcon";
 import HeightIcon from "../assets/Icons/HeightIcon";
@@ -13,16 +14,30 @@ export default function Home() {
     <div className={styles.container}>
       <Header />
       <div className={styles.content}>
-        <div className={styles.personalize_button}>
-          <Button icon={MaleIcon} />
-          <Button icon={HeightIcon} text={"168 CM"} />
-          <Button icon={WeightIcon} text={"65 KG"} />
+        <div className={styles.data_container}>
+          <div className={styles.chart_container}>
+            <Chart height={100 / 3 + "%"} width="95%" />
+            <Chart height={100 / 3 + "%"} width="95%" />
+            <Chart height={100 / 3 + "%"} width="95%" />
+          </div>
+          <div className={styles.data_section}>
+            <div className={styles.personalize_button}>
+              <Button icon={MaleIcon} />
+              <Button icon={HeightIcon} text={"168 CM"} />
+              <Button icon={WeightIcon} text={"65 KG"} />
+            </div>
+            <div className={styles.realtime}>
+              <RealTimeBox title={"PRESSURE"} data={45} unit={"mmHg"} />
+              <RealTimeBox title={"FLOW RATE"} data={100} unit={"SLPM"} />
+              <RealTimeBox title={"VOLUME"} data={500} unit={"ml"} />
+              <RealTimeBox title={"OXYGEN"} data={22} unit={"%"} />
+            </div>
+          </div>
         </div>
-        <div className={styles.realtime}>
-          <RealTimeBox title={"PRESSURE"} data={45} unit={"mmHg"} />
-          <RealTimeBox title={"FLOW RATE"} data={100} unit={"SLPM"} />
-          <RealTimeBox title={"VOLUME"} data={500} unit={"ml"} />
-          <RealTimeBox title={"OXYGEN"} data={22} unit={"%"} />
+        <div className={styles.control_container}>
+          <Button icon={HeightIcon} text={"START"} />
+          <Button icon={HeightIcon} text={"SETTINGS"} />
+          <Button icon={HeightIcon} text={"DATA"} />
         </div>
       </div>
       <Footer />
